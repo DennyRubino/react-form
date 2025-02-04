55fdfc376f5edcba6f72da436d83c27b78f611cd
Denny Rubino <rubidenny@gmail.com>
2025-02-04 16:58:12 +0100

    add promps

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import articlesData from "./assets/Data/ArticleData";
 import "./App.css";
 
 function App() {
+  const [articles, setArticles] = useState(articlesData);
+  const [newArticleTitle, setNewArticleTitle] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newArticle = { id: Date.now(), title: newArticleTitle };
+    setArticles([...articles, newArticle]);
+    setNewArticleTitle("");
+  };
+
   return (
-    <div className="App container-app">
+    <div className="App">
       <h1>Lista Articoli</h1>
       <ul>
-        {articlesData.map((article) => (
+        {articles.map((article) => (
           <li key={article.id}>
             <h2>{article.title}</h2>
           </li>
         ))}
       </ul>
+
+      <h2>Aggiungi un nuovo articolo</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Titolo dell'articolo"
+          value={newArticleTitle}
+          onChange={(e) => setNewArticleTitle(e.target.value)}
+        />
+        <button type="submit">Aggiungi Articolo</button>
+      </form>
     </div>
   );
 }
--- react-form/src/assets/Data/ArticleData.jsx
+++ react-form/src/assets/Data/ArticleData.jsx
@@ -2,6 +2,7 @@ const articlesData = [
   { id: 1, title: "Primo Articolo" },
   { id: 2, title: "Secondo Articolo" },
   { id: 3, title: "Terzo Articolo" },
+  { id: 4, title: "Quarto Articolo" },
 ];
 
 export default articlesData;
