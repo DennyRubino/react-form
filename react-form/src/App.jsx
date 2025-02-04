58c42c4842e5c5b357e39816ed9b00a865f0e0e5
Denny Rubino <rubidenny@gmail.com>
2025-02-04 16:10:54 +0100

    Add article

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from "react";
-import Article from "./assets/Data/ArticleData";
+import React from "react";
+import articlesData from "./assets/Data/ArticleData";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container-app">
       <h1>Lista Articoli</h1>
       <ul>
         {articlesData.map((article) => (
-          <li key={article.id}>{article.title}</li>
+          <li key={article.id}>
+            <h2>{article.title}</h2>
+          </li>
         ))}
       </ul>
     </div>
   );
 }
-
-import "./App.css";
 
 export default App;
--- react-form/src/assets/Data/ArticleData.jsx
+++ react-form/src/assets/Data/ArticleData.jsx
@@ -1,9 +1,7 @@
-function Article() {
-  const campioni = [
-    { id: 1, title: "Primo Articolo" },
-    { id: 2, title: "Secondo Articolo" },
-    { id: 3, title: "Terzo Articolo" },
-  ];
-}
+const articlesData = [
+  { id: 1, title: "Primo Articolo" },
+  { id: 2, title: "Secondo Articolo" },
+  { id: 3, title: "Terzo Articolo" },
+];
 
-export default Article;
+export default articlesData;
